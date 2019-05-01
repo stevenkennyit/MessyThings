f2146d891babcc93b69a076ebad2e06414a0fa0d
Steven <stevenkenny.it@gmail.com>
2019-05-01 21:00:55 +0100

    Create Search-Registry.ps1

---
+++ Search-Registry.ps1
@@ -0,0 +1,13 @@
+#Command: regsearch,<searchBase>, <key/value>
+function Search-Registry($arg1, $arg2){
+$searchBase = $arg1
+    $keyValue = $arg2
+    cls; Write-Host "Searching" $searchBase "for" $keyValue "(Slow)" -ForegroundColor Yellow
+    $res = gci $searchBase -Recurse -ErrorAction SilentlyContinue | ? {$_.property -eq $keyValue} 
+    if($res){
+        write-host "Found key" $keyValue "here:" $res.Name -ForegroundColor Green
+    }
+    else{
+        Write-Host "Key" $keyValue "not found!" -ForegroundColor Red
+    }
+}
