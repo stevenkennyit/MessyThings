01e35360df9828ea128cc4ccec726fd2247fa8e6
Steven <stevenkenny.it@gmail.com>
2019-05-02 14:18:05 +0100

    Update Search-Registry.ps1

--- Search-Registry.ps1
+++ Search-Registry.ps1
@@ -2,7 +2,7 @@
 function Search-Registry($arg1, $arg2){
 $searchBase = $arg1
     $keyValue = $arg2
-    cls; Write-Host "Searching" $searchBase "for" $keyValue "(Slow)" -ForegroundColor Yellow
+    #cls; Write-Host "Searching" $searchBase "for" $keyValue "(Slow)" -ForegroundColor Yellow
     $res = gci $searchBase -Recurse -ErrorAction SilentlyContinue | ? {$_.property -eq $keyValue} 
     if($res){
         write-host "Found key" $keyValue "here:" $res.Name -ForegroundColor Green
