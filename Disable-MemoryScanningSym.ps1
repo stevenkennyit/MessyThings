fa4448ee01e9103d22944a74a2aa470bced76da7
Steven <stevenkenny.it@gmail.com>
2019-05-07 15:52:21 +0100

    Update Disable-MemoryScanningSym.ps1

--- Disable-MemoryScanningSym.ps1
+++ Disable-MemoryScanningSym.ps1
@@ -1,10 +1,11 @@
-#Disable-MemoryScanningSym
+#DisableMemoryScanningSym
+#Command: regsearch,<searchBase>, <key/value>
 function Disable-MemoryScanningSym(){
     $searchBase = "HKLM:\SOFTWARE\WOW6432Node\Symantec\Symantec Endpoint Protection\AV\Storages\Filesystem\RealTimeScan" 
-    $keyValue = "DeferredScanning"
+    $keyValue = "XXXXXXXXDeferredScanning"
     cls; Write-Host "Adding" $keyValue "with value of 0 to" $searchBase -ForegroundColor Yellow
     try{
-        Set-ItemProperty -Path $searchBase -Name $keyValue -Value 0
+        Set-ItemProperty -Path $searchBase -Name $keyValue -Value 0 -ErrorAction Stop
         Write-Host "Success!"
     }
     catch{
